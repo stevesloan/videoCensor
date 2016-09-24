40b596c12d3096eef9860bb9a75baadd4e30bad2
Steve Sloan <sloan.steve@gmail.com>
2016-09-23 21:58:22 -0400

    got data into object

--- test.py
+++ test.py
@@ -2,6 +2,7 @@
 
 import os
 import subprocess
+from functools import reduce
 
 # answer = input("What would you like to do? ")
 
@@ -11,6 +12,11 @@ output = output.decode("utf-8")
 
 arList = output.split('\n| + A track')
 
+
+def test1():
+    def test2(inp):
+        return inp + 'test2'
+    return 'test1'
 
 
 
@@ -24,24 +30,37 @@ arList = list(filter((lambda x: filterString('subtitles', x)),arList))
 def splitStringColumns(listItem):
     out = listItem.split(':');
     if len(out) == 2:
-        return {out[0]:out[1]}
+        return [out[0],out[1]]
     else:
-        return False;
+        return False
 
+def arrayToObject(list):
+    obj = {};
+    for item in list:
+        if(item):
+            obj.update({item[0]:item[1]})
+    return obj;
 
 
 def splitString(listItem):
     out = listItem.split('\n|  + ');
     out = list(map(splitStringColumns,out));
+    out = arrayToObject(out);
+    print(out)
     return out;
+
+
 
 arList = list(map(splitString,arList));
 
 
-# def arrayToObject(item):
 
 
-
+# oList = reduce(lambda x,y: dict(x.items() + { y[0] : y[1]}), arList);
+# oList = {x[0] : x[2] for x in arList}
+# oList = dict(map(lambda x: [x[0], x[1]], arList))
+# print(oList)
+# obj.update(add_obj)
 
 
 
@@ -50,4 +69,5 @@ arList = list(map(splitString,arList));
 def e(x):
     # out = list(filter())
     print(x)
+    # print(arrayToObject(x))
 list(map(e,arList))
